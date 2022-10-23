4834f361c9baa17fb1473334633a42d86904a1df
Preshfile <preshcharlies2014@gmail.com>
2022-10-23 16:42:39 +0100

    complete

---
+++ Day_01/Exercises/Exercise_01/exercise_01.py
@@ -0,0 +1,1 @@
+print("Day 1 - Python Print Function\n The function is declared like this:\n print('what to print')")
---
+++ Day_01/Exercises/Exercise_03/exercise_03.py
@@ -0,0 +1,2 @@
+#This code prints the length(number of characters) of a string
+print(len(input('What is your name?\n')) )
